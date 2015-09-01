0cc6ec3b48d6416456dfae18e55264d1c2461f1a
albert <zenga@vmware.com>
2015-09-01 14:17:27 +0800

    add vector unitest

--- vector/main.cc
+++ vector/main.cc
@@ -1,39 +1,49 @@
 #include <stdlib.h>
 #include "qh_vector.h"
-#include <assert.h>
 #include <stdio.h>
-
+#include <assert.h>
+#include <iostream>
+using namespace std;
 void test1()       //push_back pop_back size   resize reverse
 {
 	qh::vector<int> v;
-	v.push_back(1);
-	printf("%d\n", v.capacity());
-	int size1 = v.size();
-	assert(size1 == 1);
-	int num = 0;
-	v.pop_back(num);
-	assert(num == 2);
-	size1  = v.size();
-	assert(size1 == 0);
+	v.push_back(1);              //push_back
+	assert(v[0]==1);
+	assert(v.size() == 1);
+	v.pop_back();                //pop_back
+	assert(v.size() == 0);
 	v.push_back(2);
 	v.push_back(3);
-	printf("%d\n", v.capacity());
-	num = v[0];
-	assert(num == 2);
-	v.resize(1);
-	assert(size1 == 1);
-
+	assert(v.size() == 2);
+	v.resize(1);               //resize
+	assert(v.size() == 1);
+	v.push_back(5);
+	v.push_back(10);
+	assert(v.capacity()==4);
+	qh::vector<int> v2(10,8);  //constructor
+//	cout<<v2.capacity()<<endl;
+	assert(v2.capacity()==10);
+	assert(v2.size()==10);
+	assert(v2[9] == 8);          //operator[]
+	v2.push_back(100);           // add size and capacity
+	assert(v2.capacity() == 20);  //capacity double
+	assert(v2.size()==11);
+//	cout<<v2[10]<<endl;
+	assert(v2[10] == 100);        
+	cout<<"ALL the test1 is OK!"<<endl;
+	
 
 }
+
 
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
     //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
 
-    qh::vector<int> num_vect;
-    num_vect.push_back(1);
-    test1();
+ //   qh::vector<int> num_vect;
+//    num_vect.push_back(1);
+      test1();
 #ifdef WIN32
     system("pause");
 #endif
--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -15,14 +15,12 @@ namespace qh
         explicit vector( size_t n, const T& value = T())
         {
             data_ = new T[n];
-            size_ = n;
-            capacity_ = n;
-
+	    size_ = n;
+	    capacity_ = n;
             for (size_t i = 0; i < n; i++)
             {
                 data_[i] = value;
             }
-
         }
 
         vector<T>& operator=(const vector<T>& rhs)
@@ -53,27 +51,95 @@ namespace qh
         {
             return size_;
         }
-
-        size_t capacity() const
+	
+	size_t capacity() const
         {
             return capacity_;
         }
 
         // set & get
-        T& operator[](size_t index);
+        T& operator[](size_t index) 
+        {
+	    return data_[index];
+        }
 
         // set
-        void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize(size_t size, const T& value = T());
-        void reserve(size_t capacity_);
-        void clear();
-        bool empty();
+        void push_back(const T& element)
+        {
+	   if(size_ == capacity_)
+	   {
+		 if(size_ == 0)
+			reserve(1);
+		 else
+		 	reserve(2 * capacity_);
+	   }
+           data_[size_] = element;
+           size_++;
+	}
+        void pop_back()
+	{
+            size_ --;
+	}
+        void resize(size_t size1, const T& value = T())
+	{
+	    if(size1 < size_)
+            {
+                        T* temp = new T[size1];
+                        for(int i = 0;i < size1; i++) {
+                                temp[i] = data_[i];
+                        }
+                        T* temp1 = temp;
+                        temp = data_;
+                        data_ = temp1;
+                        delete[] temp;
+                        size_ = size1;
+            }else
+            {
+
+                        T* temp = new T[size1];
+                        for(size_t i = 0;i < size_; i++) {
+                                temp[i] = data_[i];
+                        }
+                        for(size_t j = size_;j<size1;j++) {
+                                temp[j] = value;
+                        }
+                        T* temp1 = temp;
+                        temp = data_;
+                        data_ = temp1;
+                        delete[] temp;
+                        size_ = size1;
+                        capacity_ = size1;  
+             }
+	}
+        void reserve(size_t cap)
+	{
+		 if (cap > capacity_)
+                {
+                        T* temp = new T[cap];
+                        for(int i =0;i < size_; i++) {
+                                temp[i] = data_[i];
+                        }   
+                        capacity_ = cap;
+                        T* temp1 = temp;
+                        temp = data_;
+                        data_ = temp1;
+                        delete[] temp;
+                }
+	}
+        void clear()
+	{
+		size_ = 0;
+                capacity_ = 0;
+	}
+        bool empty()
+	{
+		return (size_ == 0);
+	}
 
     private:
         T*      data_;
         size_t  size_;
-        size_t  capacity_;  // number of elements that can be stored
+	size_t  capacity_;
     };
 }
 
